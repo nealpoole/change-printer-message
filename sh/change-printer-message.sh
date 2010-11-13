f09e9bb3c3dce5df357f1bfff70ea28620260a16
Neal Poole <neal@nealpoole.com>
2010-11-12 23:32:57 -0500

    Added arg count check to sh example.

--- sh/change-printer-message.sh
+++ sh/change-printer-message.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+if [ "$#" -ne 2 ]; then
+  echo "ERROR: Need to specify both a printer address and a message"
+  exit 0
+fi
+
 echo -e "\x1B%-12345X@PJL RDYMSG DISPLAY = \"$2\"\r\n\x1B%-12345X\r\n" | nc -w 1 $1 9100
